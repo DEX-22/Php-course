0c961bcedc8f51f619939e8eefdfe6dd3de58f84
Enrique Saenz <enriquesaenz0@gmail.com>
2022-11-06 13:45:27 -0500

    .

--- teoria/classes/index.php
+++ teoria/classes/index.php
@@ -1,5 +1,36 @@
 <?php
 
-    
+    // Declaracion y nombre de la clase
+
+    class Ejemplo{
+
+        // Propiedades de la clase
+
+        public $name;
+        public $date;
+
+        // Contructor
+
+        public function __construct($name)
+        {
+            $this->name = $name;
+        }
+
+        // Methods
+
+        public function setDate($date){
+            $this->date = $date;
+        }
+
+        public function getDate(){
+            return $this->date;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+
+    }
 
 ?>
